5c4927eabb90d037e68656c11c1182169773cb6e
Jeff Sutherland <jdsuther@asu.edu>
2017-02-03 12:33:20 +0700

    Add anagram

---
+++ anagram/anagram.js
@@ -0,0 +1,48 @@
+class Anagram {
+  constructor(subject) {
+    this.subject = subject;
+  }
+
+  matches(...candidates) {
+    if (this.hasArrayArguments(candidates[0])) {
+      candidates = candidates[0];
+    }
+
+    let anagrams = [];
+
+    candidates.forEach((candidate) => {
+      if (this.isAnagram(candidate)) {
+        anagrams.push(candidate);
+      }
+    });
+
+    return anagrams;
+  }
+
+  hasArrayArguments(args) {
+    return typeof args === 'object';
+  }
+
+  isAnagram(candidate) {
+    return this.hasMatchingLengths(candidate) &&
+      !this.subjectSameCandidate(candidate) &&
+      this.hasSameCharacters(candidate);
+  }
+
+  hasMatchingLengths(candidate) {
+    return this.subject.length === candidate.length;
+  }
+
+  subjectSameCandidate(candidate) {
+    return this.subject === candidate.toLowerCase();
+  }
+
+  hasSameCharacters(candidate) {
+    const subjectLetters = this.subject.toLowerCase().split('').sort().toString();
+    const candidateLetters = candidate.toLowerCase().split('').sort().toString();
+
+    return subjectLetters === candidateLetters;
+  }
+}
+
+module.exports = Anagram;
