fbce7904d626019b14fd7435b3c7cda6766715f9
Jeff Sutherland <jdsuther@asu.edu>
2017-02-12 12:02:23 +0700

    Remove quotes from object properties in food-chain

--- food-chain/food-chain.js
+++ food-chain/food-chain.js
@@ -1,13 +1,13 @@
 const FoodChain = module.exports = {
   FOOD_CHAIN: {
-    'fly': "",
-    'spider': "It wriggled and jiggled and tickled inside her.\n",
-    'bird': "How absurd to swallow a bird!\n",
-    'cat': "Imagine that, to swallow a cat!\n",
-    'dog': "What a hog, to swallow a dog!\n",
-    'goat': "Just opened her throat and swallowed a goat!\n",
-    'cow': "I don't know how she swallowed a cow!\n",
-    'horse': "She's dead, of course!\n",
+    fly: "",
+    spider: "It wriggled and jiggled and tickled inside her.\n",
+    bird: "How absurd to swallow a bird!\n",
+    cat: "Imagine that, to swallow a cat!\n",
+    dog: "What a hog, to swallow a dog!\n",
+    goat: "Just opened her throat and swallowed a goat!\n",
+    cow: "I don't know how she swallowed a cow!\n",
+    horse: "She's dead, of course!\n",
   },
 
   getKeyAtIndex(index) {
